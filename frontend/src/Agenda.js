b1d1ea1abdb14b677b0c84b3857dc6fd46f42942
estebandalelr <estebandalelr@gmail.com>
2017-10-04 14:59:21 -0500

    Update render and create renderFriends

--- frontend/src/Agenda.js
+++ frontend/src/Agenda.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Friend from "./Friend.js"
 
 class Agenda extends Component {
   constructor(props) {
     super (props);
   }
 
+  renderFriends(){
+    return this.props.friends.map(
+      (t,i)=>{
+        return <Friend friend={t} key={i}/>;
+      }
+    );
+  }
+
   render() {
     return (
-      <div>
-
-      </div>
+      <table className='table-hover thead-default table-responsive'>
+        <tbody>
+          <tr>
+            <th>Nombre</th>
+            <th>Website</th>
+          </tr>
+          {this.renderFriends()}
+        </tbody>
+      </table>
     );
   }
 }
