530cc67b83e8c4bfcda11f3f54c8230b783fada0
estebandalelr <estebandalelr@gmail.com>
2017-10-04 14:27:46 -0500

    Add constructor state, import Agenda, render Agenda

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Agenda from "./Agenda.js";
 
 class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      friends:[
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Agenda MERN</h2>
+        <Agenda friends={this.state.friends}/>
       </div>
     );
   }
