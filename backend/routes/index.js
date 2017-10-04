052bd25baea87328ad0c1113623eb63880fd34d8
estebandalelr <estebandalelr@gmail.com>
2017-10-04 11:31:17 -0500

    Require mongo, mongo url, getagenda function, router get /agenda

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mongodb =require('mongodb');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var url ="mongodb://165.227.187.208:4536/";
+
+function getagenda(callback){
+  mongodb.connect(url, (err,db) => {
+    if (err) throw err;
+
+    var agenda =  db.collection("agenda");
+    agenda.find({}).toArray((err2, agenda)=>{
+      if (err2) throw err2;
+
+      callback(agenda);
+      
+    });
+  });
+}
+
+/* GET agenda */
+router.get('/agenda', function(req, res) {
+  getagenda((agenda) => {
+    res.json(agenda);
+  });
 });
 
 module.exports = router;
