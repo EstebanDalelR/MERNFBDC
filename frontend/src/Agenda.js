55f6c24c422534813a810421654a180bec841d4a
estebandalelr <estebandalelr@gmail.com>
2017-10-04 14:33:00 -0500

    Basic component structure

---
+++ frontend/src/Agenda.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class agenda extends Component {
+  constructor(props) {
+    super (props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Agenda;
